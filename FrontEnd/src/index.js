741462adb5731bda05fec100c0421616f016ae49
daddyjanno <jn.drugmand@gmail.com>
2025-01-24 23:38:07 +0100

    index.js

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,4 +1,5 @@
 import { createFigure } from './js/createFigure.js'
+import { createFilter } from './js/createFilter.js'
 import { fetchCategories, fetchWorks } from './js/fetchData.js'
 
 const BASEURL = 'http://localhost:5678/api/'
@@ -10,8 +11,13 @@ const categories = await fetchCategories(BASEURL)
 console.log(categories)
 
 const gallery = document.querySelector('.gallery')
-
 works.forEach((work) => {
     const figure = createFigure(work)
     gallery.appendChild(figure)
+})
+
+const filters = document.querySelector('.filters')
+categories.forEach((category) => {
+    const filter = createFilter(category)
+    filters.appendChild(filter)
 })
