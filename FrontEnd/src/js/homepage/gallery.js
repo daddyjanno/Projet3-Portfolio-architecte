91fa5209c88fd736d0fff0a47064dcf3a6c763c5
daddyjanno <jn.drugmand@gmail.com>
2025-01-26 16:00:37 +0100

    import variables

--- FrontEnd/src/js/homepage/gallery.js
+++ FrontEnd/src/js/homepage/gallery.js
@@ -1,14 +1,13 @@
+import { GALLERY } from '../utils/variables.js'
 import { createFigure } from './createFigure.js'
 
-const gallery = document.querySelector('.gallery')
-
 export function clearGallery() {
-    document.querySelector('.gallery').innerHTML = ''
+    GALLERY.innerHTML = ''
 }
 
 export function displayWorks(works) {
     works.forEach((work) => {
         const figure = createFigure(work)
-        gallery.appendChild(figure)
+        GALLERY.appendChild(figure)
     })
 }
