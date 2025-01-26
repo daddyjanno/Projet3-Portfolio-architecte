09ac21d446d29318c38a706301019f096bb85a36
daddyjanno <jn.drugmand@gmail.com>
2025-01-26 16:01:38 +0100

    index.js refacto

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,29 +1,23 @@
-import { fetchCategories, fetchWorks } from './js/utils/fetchData.js'
 import { displayFilters, manageFiltersClick } from './js/homepage/filters.js'
 import { displayWorks } from './js/homepage/gallery.js'
+import { fetchWorks, fetchCategories } from './js/utils/fetchData.js'
+import { unbold } from './js/utils/utils.js'
+import { LOGINBTN } from './js/utils/variables.js'
 
 export const BASEURL = 'http://localhost:5678/api/'
 
 const categories = await fetchCategories(BASEURL)
 const works = await fetchWorks(BASEURL)
-const loginBtn = document.getElementById('login')
-
-export function bold(element) {
-    element.classList.add('bold')
-}
-export function unbold(element) {
-    element.classList.remove('bold')
-}
 
 if (document.location.href === 'http://127.0.0.1:5500/FrontEnd/') {
     displayFilters(categories)
     displayWorks(works)
-    unbold(loginBtn)
+    unbold(LOGINBTN)
     console.log('index')
 }
 
 manageFiltersClick(works)
 
-loginBtn.addEventListener('click', () => {
+LOGINBTN.addEventListener('click', () => {
     document.location.href = './src/html/login.html'
 })
