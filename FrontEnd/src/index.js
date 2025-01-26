01e01c09802d4d665f124d28183cd0b0b7b4be38
daddyjanno <jn.drugmand@gmail.com>
2025-01-26 15:27:17 +0100

    index.js

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,17 +2,28 @@ import { fetchCategories, fetchWorks } from './js/fetchData.js'
 import { displayFilters, manageFiltersClick } from './js/filters.js'
 import { displayWorks } from './js/gallery.js'
 
-const BASEURL = 'http://localhost:5678/api/'
+export const BASEURL = 'http://localhost:5678/api/'
 
 const categories = await fetchCategories(BASEURL)
-displayFilters(categories)
-
 const works = await fetchWorks(BASEURL)
-displayWorks(works)
+const loginBtn = document.getElementById('login')
+
+export function bold(element) {
+    element.classList.add('bold')
+}
+export function unbold(element) {
+    element.classList.remove('bold')
+}
+
+if (document.location.href === 'http://127.0.0.1:5500/FrontEnd/') {
+    displayFilters(categories)
+    displayWorks(works)
+    unbold(loginBtn)
+    console.log('index')
+}
 
 manageFiltersClick(works)
 
-const loginBtn = document.getElementById('login')
 loginBtn.addEventListener('click', () => {
     document.location.href = './src/html/login.html'
 })
