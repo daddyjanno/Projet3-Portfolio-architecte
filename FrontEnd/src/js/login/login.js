8cd35ff6ddc63114e39a98c808866bcc775afa83
daddyjanno <jn.drugmand@gmail.com>
2025-01-26 16:35:06 +0100

    delete validateForm function and manage loginBtn

--- FrontEnd/src/js/login/login.js
+++ FrontEnd/src/js/login/login.js
@@ -1,7 +1,7 @@
 import { BASEURL } from '../../index.js'
 import { fetchCredentials } from '../utils/fetchData.js'
-import { handleLoginForm, storeToken, validateForm } from './loginForm.js'
-import { bold, returnToHomePage } from '../utils/utils.js'
+import { handleLoginForm, storeToken } from './loginForm.js'
+import { bold, returnToHomePage, unbold } from '../utils/utils.js'
 import { LOGINBTN, LOGO, SUBMITBTN } from '../utils/variables.js'
 
 if (
@@ -21,12 +21,13 @@ LOGO.addEventListener('click', () => {
 async function handleSubmit(event) {
     event.preventDefault()
     const body = handleLoginForm()
-    validateForm(body.email, body.password)
     const response = await fetchCredentials(BASEURL, body)
 
     if (response.token) {
         console.log('connected')
         storeToken(response)
+        unbold(LOGINBTN)
+        LOGINBTN.innerText = 'logout'
         returnToHomePage()
     }
 }
