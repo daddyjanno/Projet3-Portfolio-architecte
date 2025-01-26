617e8aa5bdbb96135e3eed3c6e6f955f02f90e7d
daddyjanno <jn.drugmand@gmail.com>
2025-01-26 17:06:15 +0100

    index.js

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,7 +1,7 @@
 import { displayFilters, manageFiltersClick } from './js/homepage/filters.js'
 import { displayWorks } from './js/homepage/gallery.js'
 import { fetchWorks, fetchCategories } from './js/utils/fetchData.js'
-import { unbold } from './js/utils/utils.js'
+import { toggleEditionMode, unbold } from './js/utils/utils.js'
 import { LOGINBTN } from './js/utils/variables.js'
 
 export const BASEURL = 'http://localhost:5678/api/'
@@ -12,6 +12,7 @@ const works = await fetchWorks(BASEURL)
 if (document.location.href === 'http://127.0.0.1:5500/FrontEnd/') {
     if (localStorage.getItem('token')) {
         LOGINBTN.innerText = 'logout'
+        toggleEditionMode()
     }
     LOGINBTN.addEventListener('click', () => {
         if (LOGINBTN.innerText === 'login') {
@@ -20,6 +21,7 @@ if (document.location.href === 'http://127.0.0.1:5500/FrontEnd/') {
         if (LOGINBTN.innerText === 'logout') {
             localStorage.clear()
             LOGINBTN.innerText = 'login'
+            toggleEditionMode()
         }
     })
 
