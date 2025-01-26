5980cb2e5673d7d52a9c0cc15a5fb635f8c214f1
daddyjanno <jn.drugmand@gmail.com>
2025-01-26 16:01:31 +0100

    variables.js for global variables

---
+++ FrontEnd/src/js/utils/variables.js
@@ -0,0 +1,4 @@
+export const LOGO = document.getElementById('logo')
+export const LOGINBTN = document.getElementById('login')
+export const SUBMITBTN = document.querySelector('.loginForm-submitBtn')
+export const GALLERY = document.querySelector('.gallery')
