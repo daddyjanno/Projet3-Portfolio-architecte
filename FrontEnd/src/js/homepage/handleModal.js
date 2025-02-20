4afcc79b2b9d6168e9cb3c68a6a3640b1effd604
daddyjanno <jn.drugmand@gmail.com>
2025-02-20 13:01:47 +0100

    handleModal

--- FrontEnd/src/js/homepage/handleModal.js
+++ FrontEnd/src/js/homepage/handleModal.js
@@ -1,9 +1,26 @@
+import { MODAL, MODALCLOSEBTN } from '../utils/variables.js'
+
+export function toggleIsModalOpen(isModalOpen) {
+    return !isModalOpen
+}
+
 export function openModal(modal) {
     if (modal) {
         modal.style.display = 'flex'
     }
 }
-
 export function closeModal(modal) {
     modal.style.display = 'none'
+}
+export function handleModal(isModalOpen) {
+    MODALCLOSEBTN.addEventListener('click', () => closeModal(MODAL))
+    // MODALCONTENT.addEventListener('click', () => closeModal(MODAL))
+
+    if (isModalOpen) {
+        window.addEventListener('keydown', function (event) {
+            if (event.key === 'Escape') {
+                MODAL.style.display = 'none'
+            }
+        })
+    }
 }
