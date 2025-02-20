41a37c4332a30f8ac7399d1e3b4f69c4d537759c
daddyjanno <jn.drugmand@gmail.com>
2025-02-20 13:02:03 +0100

    index js

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,27 +1,36 @@
 import {
     EDITIONBTN,
+    GALLERY,
     LOGINBTN,
     MODAL,
-    MODALCLOSEBTN,
-    MODALCONTENT,
+    MODALGRID,
 } from './js/utils/variables.js'
 import { displayFilters, manageFiltersClick } from './js/homepage/filters.js'
 import { displayWorks } from './js/homepage/gallery.js'
 import { fetchWorks, fetchCategories } from './js/utils/fetchData.js'
 import { toggleEditionMode, unbold } from './js/utils/utils.js'
-import { closeModal, openModal } from './js/homepage/handleModal.js'
-
-console.log(window.location.search)
+import {
+    handleModal,
+    openModal,
+    toggleIsModalOpen,
+} from './js/homepage/handleModal.js'
+import { createImg } from './js/homepage/createFigure.js'
 
 export const BASEURL = 'http://localhost:5678/api/'
 
 const categories = await fetchCategories(BASEURL)
 const works = await fetchWorks(BASEURL)
+console.log(works[0])
 
-const hasToken = localStorage.getItem('token')
+const hasToken = localStorage.getItem('token') ? true : false
+
+let isModalOpen = false
+console.log('isModalOpen init', isModalOpen)
 
 if (document.location.href === 'http://127.0.0.1:5500/FrontEnd/') {
-    displayWorks(works)
+    console.log('hasToken', hasToken)
+
+    displayWorks(works, GALLERY)
     displayFilters(categories)
     manageFiltersClick(works)
 
@@ -42,7 +51,12 @@ if (document.location.href === 'http://127.0.0.1:5500/FrontEnd/') {
 if (hasToken) {
     LOGINBTN.innerText = 'logout'
     toggleEditionMode()
-    EDITIONBTN.addEventListener('click', () => openModal(MODAL))
-    // MODALCLOSEBTN.addEventListener('click', () => closeModal(MODAL))
-    // MODALCONTENT.addEventListener('click', () => closeModal(MODAL))
+    EDITIONBTN.addEventListener('click', () => {
+        openModal(MODAL)
+        isModalOpen = toggleIsModalOpen(isModalOpen)
+        handleModal(isModalOpen)
+    })
+    works.forEach((work) => {
+        createImg(work, MODALGRID)
+    })
 }
