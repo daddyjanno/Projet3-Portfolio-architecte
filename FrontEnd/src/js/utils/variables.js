8cb7537ce861aea155ca4138d26b9aeb302706bc
daddyjanno <jn.drugmand@gmail.com>
2025-02-20 17:25:46 +0100

    variables

--- FrontEnd/src/js/utils/variables.js
+++ FrontEnd/src/js/utils/variables.js
@@ -1,3 +1,5 @@
+export const BASEURL = 'http://localhost:5678/api/'
+
 export const LOGO = document.getElementById('logo')
 export const LOGINBTN = document.getElementById('login')
 export const SUBMITBTN = document.getElementById('loginFormSubmit')
@@ -8,3 +10,4 @@ export const MODAL = document.querySelector('.modal')
 export const MODALCLOSEBTN = document.querySelector('.modal-close')
 export const MODALCONTENT = document.querySelector('.modal-content')
 export const MODALGRID = document.querySelector('.gallery-grid')
+export const DELETEBTN = document.querySelector('.delete-btn')
