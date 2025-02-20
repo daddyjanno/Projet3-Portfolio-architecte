e57ce19c75c8261f899d884b407311d13273fb9d
daddyjanno <jn.drugmand@gmail.com>
2025-02-20 13:01:23 +0100

    createFigure

--- FrontEnd/src/js/homepage/createFigure.js
+++ FrontEnd/src/js/homepage/createFigure.js
@@ -1,14 +1,26 @@
-export function createFigure(work) {
+export function createFigure(work, element) {
     const figure = document.createElement('figure')
+    const img = createImg(work, figure)
+    const caption = createCaption(work, figure)
 
+    element.appendChild(figure)
+
+    return figure
+}
+export function createCaption(work, element) {
+    const figCaption = document.createElement('figcaption')
+    figCaption.innerText = work.title
+    element.appendChild(figCaption)
+
+    return figCaption
+}
+
+export function createImg(work, element) {
     const img = document.createElement('img')
     img.src = work.imageUrl
     img.alt = work.title
-    figure.appendChild(img)
 
-    const figCaption = document.createElement('figcaption')
-    figCaption.innerText = work.title
-    figure.appendChild(figCaption)
+    element.appendChild(img)
 
-    return figure
+    return img
 }
