2a4f2ecc5c6f8e4a5120df0cef16a268a3a7e750
daddyjanno <jn.drugmand@gmail.com>
2025-02-27 18:24:56 +0100

    login

--- FrontEnd/src/js/login/login.js
+++ FrontEnd/src/js/login/login.js
@@ -1,8 +1,9 @@
-import { BASEURL } from '../../index.js'
 import { fetchCredentials } from '../utils/data.js'
 import { handleLoginForm, storeToken } from './loginForm.js'
 import { bold, returnToHomePage, unbold } from '../utils/utils.js'
-import { LOGINBTN, LOGO, SUBMITBTN } from '../utils/variables.js'
+import { BASEURL, LOGINBTN, LOGO, SUBMITBTN } from '../utils/variables.js'
+
+console.log(window.location.pathname)
 
 if (window.location.pathname === '/FrontEnd/login.html') {
     bold(LOGINBTN)
