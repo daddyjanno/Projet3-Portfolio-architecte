b5b26c11011b1ea00b7d376bf9c9218483373363
daddyjanno <jn.drugmand@gmail.com>
2025-03-09 14:46:08 +0100

    index js

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,31 +1,37 @@
-import { EDITIONBTN, FILTERS, GALLERY, LOGINBTN } from './js/utils/variables.js'
+import {
+    CATEGORIES,
+    EDITIONBTN,
+    FILTERS,
+    GALLERY,
+    LOGINBTN,
+} from './js/utils/variables.js'
 import { displayFilters, manageFiltersClick } from './js/homepage/filters.js'
 import { displayWorks } from './js/homepage/gallery.js'
-import { fetchWorks, fetchCategories } from './js/utils/data.js'
 import { toggleEditionMode, unbold } from './js/utils/utils.js'
 import {
     displayWorksInModal,
     handleModal,
     toggleIsModalOpen,
 } from './js/homepage/handleModal.js'
+import { fetchWorks } from './js/utils/data.js'
 
-console.log('index')
-export const categories = await fetchCategories()
 const works = await fetchWorks()
 
-const hasToken = localStorage.getItem('token') ? true : false
+function init() {
+    console.log('index')
+    console.log(works)
+    let isModalOpen = false
+    const hasToken = localStorage.getItem('token') ? true : false
 
-let isModalOpen = false
+    console.log(isModalOpen, hasToken)
 
-if (window.location.pathname === '/FrontEnd/') {
-    displayWorks(works, GALLERY)
-    displayFilters(categories)
+    displayWorks(GALLERY, works)
+    displayFilters(CATEGORIES)
     manageFiltersClick(works)
 
     LOGINBTN.addEventListener('click', () => {
         if (LOGINBTN.innerText === 'login') {
             document.location.href = './login.html'
-            FILTERS.style.display = 'none'
         }
         if (LOGINBTN.innerText === 'logout') {
             FILTERS.style.display = 'flex'
@@ -35,15 +41,18 @@ if (window.location.pathname === '/FrontEnd/') {
             unbold(LOGINBTN)
         }
     })
-}
-if (hasToken) {
-    FILTERS.style.display = 'none'
-    LOGINBTN.innerText = 'logout'
-    toggleEditionMode()
-    EDITIONBTN.addEventListener('click', () => {
-        isModalOpen = toggleIsModalOpen(isModalOpen)
-        handleModal(isModalOpen)
 
-        displayWorksInModal(works)
-    })
+    if (hasToken) {
+        FILTERS.style.display = 'none'
+        LOGINBTN.innerText = 'logout'
+        toggleEditionMode()
+        EDITIONBTN.addEventListener('click', () => {
+            isModalOpen = toggleIsModalOpen(isModalOpen)
+            handleModal(isModalOpen, works)
+
+            displayWorksInModal(works)
+        })
+    }
 }
+
+init()
