456a30ed3857b6be228fd7f2870048ff44c6aff5
daddyjanno <jn.drugmand@gmail.com>
2025-03-22 09:00:20 +0100

    gallery

--- FrontEnd/src/js/homepage/gallery.js
+++ FrontEnd/src/js/homepage/gallery.js
@@ -1,4 +1,4 @@
-import { GALLERY, WORKS } from '../utils/variables.js'
+import { GALLERY, MODALGRID } from '../utils/variables.js'
 import { createFigure, createFigureInModal } from './createFigure.js'
 
 export function clearGallery() {
@@ -9,12 +9,16 @@ export function displayWork(work) {
     const figure = createFigure(work)
     GALLERY.innerHTML += figure
 }
+export function displayWorkInModal(work) {
+    const figure = createFigureInModal(work)
+    MODALGRID.appendChild(figure)
+}
 
 export function initDisplay(works) {
     GALLERY.innerHTML = ''
 
     works.forEach((element) => {
         displayWork(element)
-        createFigureInModal(element)
+        displayWorkInModal(element)
     })
 }
