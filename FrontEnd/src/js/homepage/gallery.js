13b5a66d757383c2e8a0f9f0445c54c8047d57e9
daddyjanno <jn.drugmand@gmail.com>
2025-03-22 07:46:19 +0100

    gallery

--- FrontEnd/src/js/homepage/gallery.js
+++ FrontEnd/src/js/homepage/gallery.js
@@ -1,15 +1,20 @@
-import { GALLERY } from '../utils/variables.js'
-import { createFigure } from './createFigure.js'
+import { GALLERY, WORKS } from '../utils/variables.js'
+import { createFigure, createFigureInModal } from './createFigure.js'
 
 export function clearGallery() {
     GALLERY.innerHTML = ''
 }
 
-export function displayWorks(element, works, caption, deleteBtn) {
-    element.innerHTML = ''
+export function displayWork(work) {
+    const figure = createFigure(work)
+    GALLERY.innerHTML += figure
+}
 
-    works.forEach((work) => {
-        const figure = createFigure(work, element, work.id, caption, deleteBtn)
-        element.appendChild(figure)
+export function initDisplay(works) {
+    GALLERY.innerHTML = ''
+
+    works.forEach((element) => {
+        displayWork(element)
+        createFigureInModal(element)
     })
 }
